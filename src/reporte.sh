8dfd9611b40fe4222a2376d612f9a9eb8087c37f
Roberto Zuniga <robzuniga@mac.com>
2016-11-05 12:17:43 -0600

    corrijo gitignore y agrego mi reporte.sh

--- src/reporte.sh
+++ src/reporte.sh
@@ -2,5 +2,17 @@
 
 REPORTE=reporte-`date +%Y%m%d`.log
 
+echo "####################" >> $REPORTE
 echo "Reporte generado por $USER:" > $REPORTE
-echo " " >>$ $REPORTE
+echo " " >> $REPORTE
+echo " " >> $REPORTE
+
+echo "Interfaces disponibles en el servidor" >> $REPORTE
+echo "" >> $REPORTE
+ifconfig >> $REPORTE
+
+netstat -latup | grep LISTEN >> $REPORTE
+
+
+
+
